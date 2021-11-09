60439f041d9406b59d9016086bdf2643223e52b0
Diana Dinh-Andrus <diana.dinhandrus@revature.net>
2021-11-09 16:37:29 -0600

    auto-genereated standard code for models

--- src/main/java/com/revature/models/Profile.java
+++ src/main/java/com/revature/models/Profile.java
@@ -2,6 +2,8 @@ package com.revature.models;
 
 import com.revature.utilites.SecurityUtil;
 
+import java.util.Objects;
+
 public class Profile {
     private int pid;
     private String username;
@@ -21,5 +23,30 @@ public class Profile {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Profile{" +
+                "pid=" + pid +
+                ", username='" + username + '\'' +
+                ", passkey='" + passkey + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Profile)) return false;
+        Profile profile = (Profile) o;
+        return pid == profile.pid && Objects.equals(username, profile.username) && Objects.equals(passkey, profile.passkey) && Objects.equals(firstName, profile.firstName) && Objects.equals(lastName, profile.lastName) && Objects.equals(email, profile.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pid, username, passkey, firstName, lastName, email);
     }
 }
