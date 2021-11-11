f463902bf899298965867047d785a93c9ab9f4ab
CJPete22 <cjpeterson7@wisc.edu>
2021-11-11 09:43:43 -0600

    initial aspect commit

--- src/main/java/com/revature/aspects/AuthAspect.java
+++ src/main/java/com/revature/aspects/AuthAspect.java
@@ -1,4 +1,57 @@
 package com.revature.aspects;
 
+import com.revature.models.Profile;
+import com.revature.utilites.SecurityUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+
+@Aspect
 public class AuthAspect {
+    private static final Logger logger = LogManager.getLogger(AuthAspect.class);
+
+    @Around("execution(* com.revature.controllers.*.*(..))" +
+            "&& !@annotation(com.revature.aspects.annotations.NoAuthIn)" +
+            "&& !@target(com.revature.aspects.annotations.NoAuthIn)")
+    public ResponseEntity<?> authenticateToken(ProceedingJoinPoint pjp) throws Throwable {
+        HttpServletRequest request =
+                ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        String token = request.getHeader("Authorization");
+        if (token == null) {
+            logger.warn("No Authorization Token Received");
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        } else {
+            Profile profile = SecurityUtil.validateToken(token);
+            if (profile == null) {
+                logger.warn("Received Invalid Token");
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            } else {
+                logger.info("Received Valid Token");
+                request.setAttribute("profile", profile);
+                return (ResponseEntity<?>) pjp.proceed();
+            }
+        }
+    }
+
+    @AfterReturning(pointcut = "within(com.revature.controllers.*)", returning = "response")
+    public ResponseEntity<?> exposeHeaders(ResponseEntity<?> response) throws Throwable {
+
+        if (response != null && !response.getHeaders().keySet().isEmpty()) {
+            HttpHeaders headers = HttpHeaders.writableHttpHeaders(response.getHeaders());
+            headers.setAccessControlExposeHeaders(new ArrayList<>(response.getHeaders().keySet()));
+            return new ResponseEntity<>(response.getBody(), headers, response.getStatusCode());
+        }
+        return response;
+    }
 }
--- src/main/java/com/revature/aspects/LoggerAspect.java
+++ src/main/java/com/revature/aspects/LoggerAspect.java
@@ -1,4 +1,33 @@
 package com.revature.aspects;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Aspect
 public class LoggerAspect {
+    private static final Logger logger = LogManager.getLogger(LoggerAspect.class);
+
+    @AfterReturning(pointcut = "within(com.revature.controllers.*)", returning = "response")
+    public void log(JoinPoint jp, ResponseEntity<?> response){
+        HttpServletRequest request =
+                ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        if (response.getStatusCodeValue() >= 400) {
+            logger.warn(jp.getSignature().getDeclaringTypeName().split("\\.")[3] +
+                    " resolved " + request.getMethod() +
+                    " returning status code " + response.getStatusCode());
+        } else {
+            logger.info(jp.getSignature().getDeclaringTypeName().split("\\.")[3] +
+                    " successfully resolved " + request.getMethod() +
+                    " with status code " + response.getStatusCode());
+        }
+    }
+
 }
