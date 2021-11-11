e07ae776c936938807b48a31eb115ba47f1fc850
kalebconner408 <kalebconner408@gmail.com>
2021-11-11 16:00:23 -0600

    Implement ProfileService

--- src/main/java/com/revature/controllers/ProfileController.java
+++ src/main/java/com/revature/controllers/ProfileController.java
@@ -5,11 +5,13 @@ import com.revature.services.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
+@Component
 public class ProfileController {
 
     @Autowired
--- src/main/java/com/revature/services/ProfileServiceImpl.java
+++ src/main/java/com/revature/services/ProfileServiceImpl.java
@@ -4,24 +4,24 @@ import com.revature.models.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ProfileServiceImpl {
+public class ProfileServiceImpl implements ProfileService{
 
 
-    Profile getProfileByCredential(Profile profile)
+    public Profile getProfileByCredential(Profile profile)
     {
         return null;
     }
 
-    Profile login(String email, String password){
+    public Profile login(String email, String password){
         return null;
     }
 
-    Profile getProfileById(int pid)
+    public Profile getProfileById(int pid)
     {
         return null;
     }
 
-    Profile getProfileByEmail(String email)
+    public Profile getProfileByEmail(String email)
     {
         return null;
     }
