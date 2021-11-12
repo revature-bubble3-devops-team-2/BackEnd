90eb24632414fd723456af3fe76622225a4a0d98
Zhen Wen Wang <iamzhen13@gmail.com>
2021-11-12 11:07:11 -0800

    stuff

--- src/main/java/com/revature/utilites/HibernateUtil.java
+++ src/main/java/com/revature/utilites/HibernateUtil.java
@@ -2,13 +2,13 @@
 package com.revature.utilites;
 
 import com.revature.models.*;
-import org.apache.maven.model.Model;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 
 import java.util.Properties;
+
 
 public class HibernateUtil {
 
