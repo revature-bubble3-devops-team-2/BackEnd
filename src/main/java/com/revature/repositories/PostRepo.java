38a84cb7008e3997dd05cf876c9cb5c556525f73
henry ramirez <you@example.com>
2021-11-12 07:57:35 -0800

    removed post for dev-register

--- src/main/java/com/revature/repositories/PostRepo.java
+++ src/main/java/com/revature/repositories/PostRepo.java
@@ -1,12 +1,5 @@
 package com.revature.repositories;
 
-import com.revature.models.Post;
-import com.revature.models.Profile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface PostRepo extends JpaRepository<Post, Integer> {
-//    public List<Post> getall();
+public class PostRepo {
 
 }
--- src/main/java/com/revature/services/PostService.java
+++ src/main/java/com/revature/services/PostService.java
@@ -1,12 +1,5 @@
 package com.revature.services;
 
-import com.revature.models.Post;
-import com.revature.models.Profile;
-
-
-import java.util.List;
-
 public interface PostService {
-//    public List<Post> getAllPost();
 
 }
