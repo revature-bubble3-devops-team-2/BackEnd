97265c8eba4f69ae6d8c959bb93070264b3445ad
CJPete22 <cjpeterson7@wisc.edu>
2021-11-15 11:08:20 -0600

    driver updates

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -1,13 +1,24 @@
 package com.revature;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
+@Log4j2
 @SpringBootApplication
 public class Driver {
 
     public static void main(String[] args) {
-        SpringApplication.run(Driver.class, args);
+        String[] betterArgs = new String[]{
+                "spring.datasource.url=" + System.getenv("DB_URL"),
+                "spring.datasource.username=" + System.getenv("DB_USER"),
+                "spring.datasource.password=" + System.getenv("DB_PASS"),
+                "spring.datasource.driver=" + System.getenv("DB_DRIVER"),
+                "spring.datasource.dialect=" + System.getenv("DB_DIALECT")
+        };
+        SpringApplication.run(Driver.class, betterArgs);
     }
 }
