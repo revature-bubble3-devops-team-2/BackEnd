e2867af3a312616168638d7e8e2d00382fd5c7ff
Diana Dinh-Andrus <themonkey@anazopyreo.us>
2021-11-18 13:43:24 -0600

    finished resolving merge conflict

--- src/main/java/com/revature/services/ProfileService.java
+++ src/main/java/com/revature/services/ProfileService.java
@@ -3,19 +3,10 @@ package com.revature.services;
 import com.revature.models.Profile;
 
 public interface ProfileService {
-<<<<<<< HEAD
     public Profile login(String username, String password);
     public Profile addNewProfile(Profile profile);
     public Profile getProfileByEmail(Profile profile);
     public Profile getProfileByPid(Integer pid);
-    public Profile getProfileByUsername(String username);
     public Profile updateProfile(Profile profile);
     public Profile removeFollowByEmail(Profile profile, String email);
-=======
-    Profile login(String email, String password);
-    Profile addNewProfile(Profile profile);
-    Profile getProfileByEmail(Profile profile);
-    Profile getProfileByPid(Integer pid);
-    Profile updateProfile(Profile profile);
->>>>>>> main
 }
