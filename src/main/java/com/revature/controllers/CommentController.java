efafb7b780966fcb2d545a6d14ff3f03697d5508
David Abdhir <dabdhir67@gmail.com>
2021-11-22 12:09:43 -0800

    added java docs for comments

--- src/main/java/com/revature/controllers/CommentController.java
+++ src/main/java/com/revature/controllers/CommentController.java
@@ -20,16 +20,39 @@ public class CommentController {
     @Autowired
     CommentServiceImpl commentService;
 
+    /**
+     * addComment receives a request body that contains the Comment to be added.
+     *
+     * @param comment to be added by the user.
+     * @return HTTP created status and the comment when it is added successfully.
+     */
     @PostMapping
     public ResponseEntity<Comment> addComment(@RequestBody Comment comment){
         return new ResponseEntity<>(commentService.addComment(comment), HttpStatus.CREATED);
     }
 
+    /**
+     * getCommentsByPost receives a Post id in the request parameter, and returns a list of comments
+     * that belong to the specific post at hand.
+     *
+     * @param id Post id identifier to access specific object.
+     * @return HTTP accepted status and a list of comments based on Post id.
+     */
     @GetMapping
     public ResponseEntity<List<Comment>> getCommentsByPost(@RequestParam(value = "psid") Integer id){
         return new ResponseEntity<>(commentService.getCommentByPostPsid(id), HttpStatus.ACCEPTED);
     }
 
+    /**
+     * updateCommentByCid receives a request body that contains the new comment to be updated.
+     * It also receives a path variable that contains the id of the comment at hand. We set the
+     * comment id before updating to ensure a new id is not generated.
+     *
+     * @param comment Comment to be updated by user
+     * @param id id identifier to get original comment.
+     * @return HTTP created status and the updated comment if the updated comment is not null.
+     *         Http not acceptable status and null if it is.
+     */
     @PutMapping("/{id}")
     public ResponseEntity<Comment> updateCommentByCid(@RequestBody Comment comment, @PathVariable("id")int id){
         comment.setCid(id);
@@ -40,6 +63,15 @@ public class CommentController {
             return new ResponseEntity<>(null, HttpStatus.NOT_ACCEPTABLE);
     }
 
+    /**
+     * deleteCommentByCid receives a path variable that contains the id of the comment at hand.
+     * Invokes Spring Data's abstracted delete method. If delete comment was successful, returns
+     * true, false otherwise.
+     *
+     * @param id id identifier to get original comment
+     * @return HTTP ok status and true if delete was successful.
+     *         HTTP not acceptable status and false if otherwise.
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<Boolean> deleteCommentByCid(@PathVariable("id")int id){
         if(commentService.deleteCommentByCid(id))
--- src/main/java/com/revature/services/CommentServiceImpl.java
+++ src/main/java/com/revature/services/CommentServiceImpl.java
@@ -15,6 +15,16 @@ public class CommentServiceImpl implements CommentService {
     @Autowired
     CommentRepo commentRepo;
 
+    /**
+     * addComment will receive a comment to be added from the user and return a comment. Within a try block,
+     * it will catch any exception and return null. It also makes sure the comment has a profile and date, otherwise
+     * it will throw an exception. Spring Data's abstracted save method is called to add the comment to the database,
+     * and returns it.
+     *
+     * @param comment Comment to be added to the database
+     * @throw NullPointerException if null pointer error occurs
+     * @return Comment that was added or null if an exception is thrown.
+     */
     @Override
     public Comment addComment(Comment comment) {
         try {
@@ -28,12 +38,26 @@ public class CommentServiceImpl implements CommentService {
         }
     }
 
+    /**
+     * getCommentsByPost utilizes Spring Data's Reflection API to find a list of all comments by PostId.
+     *
+     * @param psid PostId identifier
+     * @return list of comments organized by PostId
+     *
+     */
     @Override
     public List<Comment> getCommentByPostPsid(Integer psid) {
         List<Comment> commentList = commentRepo.getCommentByPostPsid(psid);
         return commentList;
     }
 
+    /**
+     * updateComment utilizes Spring Data's Reflection API to find a comment by commentId. Inside an if statement,
+     * checks to see if the comment is null or not. If it's not null, save the new comment to the database.
+     *
+     * @param comment to be updated
+     * @return comment that was updated
+     */
     @Override
     public Comment updateComment(Comment comment) {
         Comment target = commentRepo.getCommentByCid(comment.getCid());
@@ -42,6 +66,13 @@ public class CommentServiceImpl implements CommentService {
         return commentRepo.save(target);
     }
 
+    /**
+     * deleteCommentByCid utilizes Spring Data's Reflection API to find a comment by id. Inside an if statement,
+     * checks to see if the comment is null or not. If it's not null, delete the comment.
+     *
+     * @param cid commentId identifier
+     * @return boolean to indicate if delete was successful or not.
+     */
     @Override
     public boolean deleteCommentByCid(Integer cid) {
         Comment deleteComment = commentRepo.getCommentByCid(cid);
@@ -53,6 +84,12 @@ public class CommentServiceImpl implements CommentService {
             return false;
     }
 
+    /**
+     * getCommentByCid utilizes Spring Data's Reflection API to return a comment based on its id.
+     *
+     * @param cid
+     * @return
+     */
     @Override
     public Comment getCommentByCid(Integer cid){
         return commentRepo.getCommentByCid(cid);
