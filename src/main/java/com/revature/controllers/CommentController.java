e7c5705a1953c0d8da95f05f8c3701d6448b5a19
Sitilac <m.calitis@gmail.com>
2021-11-23 09:42:36 -0800

    Cleaned out comments

--- src/main/java/com/revature/controllers/CommentController.java
+++ src/main/java/com/revature/controllers/CommentController.java
@@ -1,16 +1,12 @@
 package com.revature.controllers;
 
 import com.revature.models.Comment;
-import com.revature.models.Post;
-import com.revature.services.CommentService;
 import com.revature.services.CommentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.swing.text.StyledEditorKit;
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
