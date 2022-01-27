f27abe34aba6fbeb2ddd79c98cd90c9aa274cac4
johntboyle <83061760+johntboyle@users.noreply.github.com>
2022-01-27 17:18:55 -0600

    added repository annotation to commentrepo

--- src/main/java/com/revature/repositories/CommentRepo.java
+++ src/main/java/com/revature/repositories/CommentRepo.java
@@ -3,9 +3,11 @@ package com.revature.repositories;
 import com.revature.models.Comment;
 import com.revature.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CommentRepo extends JpaRepository<Comment,Integer> {
 
     public List<Comment> getCommentByPostPsid(Integer psid);
