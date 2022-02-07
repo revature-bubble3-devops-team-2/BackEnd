88d634a7515ba8ced6a112bf23dc858dc345df63
Somers B Matthews <somersbmatthews@gmail.com>
2022-02-07 15:01:05 -0600

    @SpringBootTest

--- src/test/java/com/revature/controllers/ProfileControllerTest.java
+++ src/test/java/com/revature/controllers/ProfileControllerTest.java
@@ -4,58 +4,92 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.Driver;
 import com.revature.dto.ProfileDTO;
 import com.revature.models.Profile;
 import com.revature.repositories.ProfileRepo;
+import com.revature.services.ProfileService;
+import com.revature.services.ProfileServiceImpl;
 import com.revature.utilites.SecurityUtil;
+import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
 
 import lombok.extern.log4j.Log4j2;
 
+
+//@ExtendWith(SpringExtension.class)
+//@WebMvcTest(ProfileController.class)
+//@RunWith(SpringRunner.class)
 @Log4j2
-public class ProfileControllerTest {
+@SpringBootTest(Arrays.of(SpringBootTest.WebEnvironment.MOCK), classes = Driver.class)
+@AutoConfigureMockMvc
+public class ProfileControllerTest extends AbstractTestNGSpringContextTests {
 	 private static final String USERNAME = "dummyUsername";
 	 private static final String PASSWORD = "abc123";
 	 private static final String EMAIL = "dummy@email.com";
 	 private static final boolean VERIFICATION = true;
-	 private Profile expected = new Profile();
+	 private static Profile expected = new Profile();
 
 	 private static final String USERNAME2 = "dummyUsername2";
 	 private static final String PASSWORD2 = "abc123";
 	 private static final String EMAIL2 = "dummy2@email.com";
-	 private Profile expected2 = new Profile();
+	 private static Profile expected2 = new Profile();
 	 
 	 private static final String TOKEN_NAME = "Authorization";
 	
 	 @Autowired
-	 private WebApplicationContext webApplicationContext;
+	 private static WebApplicationContext webApplicationContext;
 	 
-	 private ProfileDTO profiledto;
-	 private ProfileDTO testprofiledto;
+	 private static ProfileDTO profiledto;
+	 private static ProfileDTO testprofiledto;
 	 
-	 
-	 private MockMvc mockMvc;
+	 @Autowired
+	 private static MockMvc mockMvc;
 	  
 	 private static String baseUrl = "/profile";
 
+	 @MockBean 
+	 private static ProfileService profileServiceInterface;
+	 
 	 @Mock
 	 private ProfileRepo profileRepo;
-	 private Profile profile;
+	 
+	 @InjectMocks
+	 private ProfileServiceImpl profileService;
+	 
+	 
+	 private Profile actualProfile;
+	 
+	
 
-	 @BeforeEach
-	 void initMock() {
-	      MockitoAnnotations.openMocks(this);
+	 @BeforeAll
+	 static void initMock() {
+//	      MockitoAnnotations.openMocks(this);
 	      String name = "dummyName";
 	      String passkey = "c8ZLBnfDh3YsvZ2dW1KDWY6ZTak8+v+/L74e8Vfoydk1IfySsVCAZVKTZfrtPKodzUXEiR+69yjOz1qqf7U4rA==jnW" +
 	                "2sIxW7inUlQqGJCNrNa7Eavj5uMGQAYZ0S6xNz65p79QaOk8eZpOChJlFPvIadohhOuHg5PFGeewM2YmkVR260YPhJwK/GUR3YXs" +
@@ -93,28 +127,40 @@ public class ProfileControllerTest {
 	       testprofiledto.setLastName(name2);
 	       testprofiledto.setEmail(EMAIL2);
 	       testprofiledto.setVerification(VERIFICATION);
-	       //mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-	       ProfileController pc = new ProfileController();
-	       mockMvc = MockMvcBuilders.standaloneSetup(pc).build();
+	    log.info(webApplicationContext);
+	       mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	    //   ProfileController pc = new ProfileController();
+//	       mockMvc = MockMvcBuilders.standaloneSetup(pc).build();
 	  }
+	 
+	 @BeforeEach
+	 private void initEachMock() {
+		 MockitoAnnotations.openMocks(this);
+	 }
+	 
+	 private void generateHeaders() {
+		  Profile newProfile = profiledto.toProfile();
+		  HttpHeaders responseHeaders = new HttpHeaders();
+         String token = SecurityUtil.generateToken(new ProfileDTO(newProfile));
+         assertNotNull(SecurityUtil.validateToken(token));
+         responseHeaders.set(TOKEN_NAME, token);
+//         assertNotNull(responseHeaders);
+	 }
 	 
 	  @Test
 	  public void testRegister() throws Exception {
-		  Profile newProfile = profiledto.toProfile();
-		  HttpHeaders responseHeaders = new HttpHeaders();
-          String token = SecurityUtil.generateToken(new ProfileDTO(newProfile));
-          assertNotNull(SecurityUtil.validateToken(token));
-          responseHeaders.set(TOKEN_NAME, token);
-          assertNotNull(responseHeaders);
+		
+//		  given(profileService.addNewProfile(expected).willReturn(actualProfile);
+		//  when(profileRepo.save(expected)).thenReturn(expected);
           ObjectMapper objectMapper = new ObjectMapper();
-          String profileJSON = objectMapper.writeValueAsString(newProfile);
+          String profileJSON = objectMapper.writeValueAsString(expected);
           log.info(profileJSON);
+          
 		  mockMvc.perform(post(baseUrl + "/register")
 				  .contentType(MediaType.APPLICATION_JSON)
 				  .content(profileJSON))
 		  	.andExpect(status().isOk())
-		  	.andExpect(content().contentType("application/json"))
-		  	;
+		  	.andExpect(content().contentType("application/json"));
 		  
 	  }
 	 
