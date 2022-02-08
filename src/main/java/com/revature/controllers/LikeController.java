814826f1ba5bf481de29778730b7963fb403b78e
Kenny Phu <kphu@outlook.com>
2022-02-08 12:49:00 -0500

    remove unused import

--- src/main/java/com/revature/controllers/LikeController.java
+++ src/main/java/com/revature/controllers/LikeController.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
