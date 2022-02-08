b11a64fe22391a56636f327efbb2331ae5f98ef8
Jesse Rodriguez <robotsarecrazy64@gmail.com>
2022-02-07 17:22:37 -0700

    Remove unused Import

--- src/test/java/com/revature/controllers/ProfileControllerTest.java
+++ src/test/java/com/revature/controllers/ProfileControllerTest.java
@@ -1,7 +1,6 @@
 package com.revature.controllers;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
